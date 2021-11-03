e02168abc51bc92d4ce0b125ad9a024f62efa3cf
Climentia <brightness10.4tkt@gmail.com>
2021-11-03 14:27:54 +0900

    add

---
+++ flo_vis/flo_visualization.py
@@ -0,0 +1,171 @@
+import csv
+import glob
+import numpy as np
+from PIL import Image
+import pandas as pd
+import matplotlib.pyplot as plt
+
+parameter_list = [[1, 1000, 5], [0.5, 1000, 5], [0.1, 1000, 5], [0.05, 1000, 5], [0.01, 1000, 5], [0.005, 1000, 5]]
+mesh_range = 0.02
+year = 14
+month = 7
+day = 14
+time = 12.0
+t = str(year) + str(month) + str(day) + str(time)
+
+
+def flo_calculation():
+    print('-----flo_calculation-----')
+    for parameter in parameter_list:
+        print(parameter)
+        lambda_num = parameter[0]
+        iterate = parameter[1]
+        pyramid = parameter[2]
+        file_path = '../../prediction_data/parameter_lambda_' + str(lambda_num) + '_iterate_' + str(iterate) + '_p' + str(pyramid) + '/flo/' + str(month) + '月/' + str(month) + '月' + str(day) + '日/NV' + str(year) + str(month) + str(day) + str(time) + '_' + str(mesh_range) + '_' + str(lambda_num) + '_' + str(iterate) + '_p' + str(pyramid) + '_flo.csv'
+        with open(file_path) as f:
+            reader = csv.reader(f)
+            list = [row for row in reader]
+        write_path = 'sort_' + str(lambda_num) + '_' + str(iterate) + '_p' + str(pyramid) + '.csv'
+        with open(write_path, "w") as fw:
+            for i in range(601):
+                for j in range(431):
+                    lng = 129.6 + 0.02 * i
+                    lat = 31.2 + 0.02 * j
+                    try:
+                        flo_y = float(list[i][j*2]) * 1/50
+                        flo_x = float(list[i][(j*2)+1]) * 1/50
+                    except Exception as e:
+                        flo_y = "NaN"
+                        flo_x = "NaN"
+                        print(str(e))
+                    try:
+                        txt = str(round(lat, 2)) + "," + str(round(lng, 2)) + "," + str(flo_x) + "," + str(flo_y) + "\n"
+                    except Exception as e:
+                        txt = str(round(lat, 2)) + "," + str(round(lng, 2)) + "," + str(flo_x) + "," + str(flo_y) + "\n"
+                    fw.write(txt)
+
+
+def kanto_sort():
+    print('-----kanto_sort-----')
+    for parameter in parameter_list:
+        print(parameter)
+        lambda_num = parameter[0]
+        iterate = parameter[1]
+        pyramid = parameter[2]
+        read_path = 'sort_' + str(lambda_num) + '_' + str(iterate) + '_p' + str(pyramid) + '.csv'
+        df = pd.read_csv(read_path, header=None, names=['lat', 'lng', 'x_flo', 'y_flo'])
+        write_path = 'sort_kanto' + str(lambda_num) + '_' + str(iterate) + '_p' + str(pyramid) + '.csv'
+        with open(write_path, "w") as fw:
+            for i in range(24):
+                for j in range(24):
+                    lat = 34.8 + 0.1 * i
+                    lat_min = lat-0.05
+                    lat_max = lat+0.05
+                    lng = 138.4 + 0.1 * j
+                    lng_min = lng-0.05
+                    lng_max = lng+0.05
+                    df2 = df[(lat_min <= df['lat']) & (lat_max > df['lat']) & (lng_min <= df['lng']) & (lng_max > df['lng'])]
+                    flo_x = df2['x_flo'].mean()
+                    flo_y = df2['y_flo'].mean()
+                    txt = str(round(lat, 2)) + "," + str(round(lng, 2)) + "," + str(flo_x) + "," + str(flo_y) + "\n"
+                    fw.write(txt)
+
+
+def visualization():
+    im = Image.open("./kanto.png")
+    print('-----visualization-----')
+    count = 0
+    lat_list = []
+    lng_list = []
+    x_list = []
+    y_list = []
+    for parameter in parameter_list:
+        print(parameter)
+        lambda_num = parameter[0]
+        iterate = parameter[1]
+        pyramid = parameter[2]
+        read_path = 'sort_kanto' + str(lambda_num) + '_' + str(iterate) + '_p' + str(pyramid) + '.csv'
+        df = pd.read_csv(read_path, header=None, names=['lat', 'lng', 'x_flo', 'y_flo'])
+        lat_list.append(df['lat'].values.tolist())
+        lng_list.append(df['lng'].values.tolist())
+        x_list.append(df['x_flo'].values.tolist())
+        y_list.append(df['y_flo'].values.tolist())
+        count = count + 1
+    fig = plt.figure(figsize=(20, 15))
+    fig.patch.set_facecolor('white')
+    fig.suptitle('vectorplot:' + t)
+    # グラフ描画場所作成
+    ax0 = fig.add_subplot(231)
+    ax1 = fig.add_subplot(232)
+    ax2 = fig.add_subplot(233)
+    ax3 = fig.add_subplot(234)
+    ax4 = fig.add_subplot(235)
+    ax5 = fig.add_subplot(236)
+    # 軸ラベル設定
+    ax0.set_xlabel("longitude", fontsize=8)
+    ax0.set_ylabel("latitude", fontsize=8)
+    ax1.set_xlabel("longitude", fontsize=8)
+    ax1.set_ylabel("latitude", fontsize=8)
+    ax2.set_xlabel("longitude", fontsize=8)
+    ax2.set_ylabel("latitude", fontsize=8)
+    ax3.set_xlabel("longitude", fontsize=8)
+    ax3.set_ylabel("latitude", fontsize=8)
+    ax4.set_xlabel("longitude", fontsize=8)
+    ax4.set_ylabel("latitude", fontsize=8)
+    ax5.set_xlabel("longitude", fontsize=8)
+    ax5.set_ylabel("latitude", fontsize=8)
+    # タイトル設定
+    ax0.quiver(lng_list[0], lat_list[0], x_list[0], y_list[0], color="blue", angles='xy', scale_units='xy', scale=1)
+    ax1.quiver(lng_list[1], lat_list[1], x_list[1], y_list[1], color="blue", angles='xy', scale_units='xy', scale=1)
+    ax2.quiver(lng_list[2], lat_list[2], x_list[2], y_list[2], color="blue", angles='xy', scale_units='xy', scale=1)
+    ax3.quiver(lng_list[3], lat_list[3], x_list[3], y_list[3], color="blue", angles='xy', scale_units='xy', scale=1)
+    ax4.quiver(lng_list[4], lat_list[4], x_list[4], y_list[4], color="blue", angles='xy', scale_units='xy', scale=1)
+    ax5.quiver(lng_list[5], lat_list[5], x_list[5], y_list[5], color="blue", angles='xy', scale_units='xy', scale=1)
+    ax0.set_ylim(34.9083, 37.1570)
+    ax0.set_xlim(138.3806, 140.8588)
+    ax1.set_ylim(34.9083, 37.1570)
+    ax1.set_xlim(138.3806, 140.8588)
+    ax2.set_ylim(34.9083, 37.1570)
+    ax2.set_xlim(138.3806, 140.8588)
+    ax3.set_ylim(34.9083, 37.1570)
+    ax3.set_xlim(138.3806, 140.8588)
+    ax4.set_ylim(34.9083, 37.1570)
+    ax4.set_xlim(138.3806, 140.8588)
+    ax5.set_ylim(34.9083, 37.1570)
+    ax5.set_xlim(138.3806, 140.8588)
+    # 大きさ取得
+    xlim0 = ax0.get_xlim()
+    ylim0 = ax0.get_ylim()
+    xlim1 = ax1.get_xlim()
+    ylim1 = ax1.get_ylim()
+    xlim2 = ax2.get_xlim()
+    ylim2 = ax2.get_ylim()
+    xlim3 = ax3.get_xlim()
+    ylim3 = ax3.get_ylim()
+    xlim4 = ax4.get_xlim()
+    ylim4 = ax4.get_ylim()
+    xlim5 = ax5.get_xlim()
+    ylim5 = ax5.get_ylim()
+    # [[1, 1000, 5], [0.5, 1000, 5], [0.1, 1000, 5], [0.05, 1000, 5], [0.01, 1000, 5], [0.005, 1000, 5]]
+    # [[1, 1000, 1], [0.01, 1000, 1], [0.1, 1000, 5], [1, 1000, 5], [0.01, 1000, 5], [0.001, 1000, 5]]
+    ax0.set_title('lambda:1 iterate:1000 pyramid:5', fontsize=10)
+    ax1.set_title('lambda:0.5 iterate:1000 pyramid:5', fontsize=10)
+    ax2.set_title('lambda:0.1 iterate:1000 pyramid:5', fontsize=10)
+    ax3.set_title('lambda:0.05 iterate:1000 pyramid:5', fontsize=10)
+    ax4.set_title('lambda:0.01 iterate:1000 pyramid:5', fontsize=10)
+    ax5.set_title('lambda:0.005 iterate:1000 pyramid:5', fontsize=10)
+    ax0.imshow(im, extent=[*xlim0, *ylim0], aspect='auto', alpha=0.9)
+    ax1.imshow(im, extent=[*xlim1, *ylim1], aspect='auto', alpha=0.9)
+    ax2.imshow(im, extent=[*xlim2, *ylim2], aspect='auto', alpha=0.9)
+    ax3.imshow(im, extent=[*xlim3, *ylim3], aspect='auto', alpha=0.9)
+    ax4.imshow(im, extent=[*xlim4, *ylim4], aspect='auto', alpha=0.9)
+    ax5.imshow(im, extent=[*xlim5, *ylim5], aspect='auto', alpha=0.9)
+    fig.subplots_adjust(wspace=0.2, hspace=0.2)
+    fig.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.9)
+    # plt.show()
+    fig.savefig('vector' + t + '.png')
+
+
+flo_calculation()
+kanto_sort()
+visualization()
